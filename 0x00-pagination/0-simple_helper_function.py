37f69211c8b2e3553f4f8391314bf82963bfa00f
birukshegute <birukshegute@gmail.com>
2024-07-26 05:41:34 +0000

    0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python3
+"""
+declares a function named index_range
+"""
 
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    a function that returns a list for particular pagination parameter
+    """
+    startIndex = page * page_size
+    return startIndex - page_size, startIndex
