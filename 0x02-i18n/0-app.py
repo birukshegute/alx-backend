a7a5e0f9c5bcfef16c02ef36b72b30bf1496f7bc
birukshegute <birukshegute@gmail.com>
2024-07-30 21:14:35 +0000

    task 0 retry

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,11 +6,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    """ route index """
+@app.route('/' strict_slashes=False)
+def index() -> str:
+    """ route index from templates"""
     return render_template("0-index.html")
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
