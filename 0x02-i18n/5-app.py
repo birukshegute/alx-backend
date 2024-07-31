5d82ec7f4e5df60eda6f12725b167b31765aeeda
birukshegute <birukshegute@gmail.com>
2024-07-31 11:49:55 +0000

    task 5

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""
+Starts a Flask web application
+"""
+
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask_babel import Babel
+from typing import Dict, Union
+
+
+class Config(object):
+    """
+    Class to configure the babel app.
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Determine the best match with our supported languages.
+    """
+    locale = request.args.get('locale', '').strip()
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user(id) -> Union[Dict[str, Union[str, None]], None]:
+    """
+    Retrieves a user from users dictionary using ID
+    """
+    return users.get(int(id), None)
+
+
+@app.before_request
+def before_request():
+    """
+    Retrieves the value of the login_as and Calls the get_user function
+    """
+    setattr(g, 'user', get_user(request.args.get('login_as', 0)))
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Route index from templates
+    """
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
