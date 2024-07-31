913295db76a49b838c9eb5cb92cacb40545a7a61
birukshegute <birukshegute@gmail.com>
2024-07-31 07:33:01 +0000

    task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Starts a Flask web application
+"""
+
+from flask import Flask
+from flask import render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """
+    Class to configure the babel app.
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Route index from templates
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
