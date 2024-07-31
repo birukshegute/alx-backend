ffb30884461002b6a887b20a848d4327c16d11d5
birukshegute <birukshegute@gmail.com>
2024-07-31 12:15:20 +0000

    task 5 edited

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -26,6 +26,31 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Union[Dict[str, Union[str, None]], None]:
+    """
+    Retrieves a user from users dictionary using ID
+    """
+    id = request.args.get('login_as')
+    if id:
+        return users.get(int(id))
+    return None
+
+@app.before_request
+def before_request():
+    """
+    Retrieves the value of the login_as and Calls the get_user function
+    """
+    g.user = get_user()
+
+
 @babel.localeselector
 def get_locale() -> str:
     """
@@ -35,29 +60,6 @@ def get_locale() -> str:
     if locale and locale in Config.LANGUAGES:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
-
-
-def get_user(id) -> Union[Dict[str, Union[str, None]], None]:
-    """
-    Retrieves a user from users dictionary using ID
-    """
-    return users.get(int(id), None)
-
-
-@app.before_request
-def before_request():
-    """
-    Retrieves the value of the login_as and Calls the get_user function
-    """
-    setattr(g, 'user', get_user(request.args.get('login_as', 0)))
 
 
 @app.route('/', strict_slashes=False)
